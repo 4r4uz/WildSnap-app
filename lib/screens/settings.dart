d51026d21c0a327f58adf6b79922b7b93134d581
Ulisess3 <nicolasulises21@gmail.com>
2025-10-07 15:56:14 -0300

    nuevos archivos

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Ajustes extends StatelessWidget {
-  const Ajustes({super.key});
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
